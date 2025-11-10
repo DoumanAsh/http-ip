30313ca6cacbd928e62d3379278b1e364d054b64
DoumanAsh <douman@gmx.se>
2025-11-10 17:43:22 +0900

    Refactoring forwarded header parser

--- src/forwarded.rs
+++ src/forwarded.rs
@@ -3,7 +3,14 @@
 use core::{marker, fmt};
 use core::net::IpAddr;
 
-#[derive(PartialEq, Eq, Debug)]
+//Forwarded syntax
+//Syntax is: <entry 1>, <entry N>
+//Entry is: <key1>=<value1>;<keyN>=<valueN>
+const FORWARDED_SEP: char = ',';
+const ENTRY_SEP: char = ';';
+const PAIR_SEP: char = '=';
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 ///Parsed node of the Forwarded header
 ///
 ///See details https://datatracker.ietf.org/doc/html/rfc7239#section-4
@@ -25,6 +32,33 @@ impl<'a> ForwardedNode<'a> {
             return Self::Name(name)
         }
     }
+
+    ///Parses `Node` identifier
+    pub fn parse_node(mut node: &'a str) -> Self {
+        node = node.trim_matches('"');
+        if node.eq_ignore_ascii_case("unknown") {
+            return Self::Unknown;
+        }
+
+        if let Some(mut ipv6) = node.strip_prefix('[') {
+            if let Some(end_addr_idx) = ipv6.find(']') {
+                ipv6 = &ipv6[..end_addr_idx];
+                return Self::parse_name(ipv6);
+            } else {
+                return Self::Name(ipv6);
+            }
+        }
+
+        let mut node = node.rsplit(':');
+        let port_or_ip = node.next().unwrap();
+        let ip = if let Some(ip) = node.next() {
+            ip
+        } else {
+            port_or_ip
+        };
+
+        ForwardedNode::parse_name(ip)
+    }
 }
 
 impl fmt::Display for ForwardedNode<'_> {
@@ -38,58 +72,157 @@ impl fmt::Display for ForwardedNode<'_> {
     }
 }
 
-#[inline(always)]
-///Extracts client IPs from forwarded header
-pub fn parse_forwarded_for(value: &str) -> ForwardedNodeIter<impl Iterator<Item = &'_ str>> {
-    ForwardedNodeIter {
-        //Syntax is: <entry 1>, <entry N>
-        //Entry is: <key1>=<value1>;<keyN>=<valueN>
-        components: value.split([';', ',']),
-        _lifetime: marker::PhantomData,
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+///`Forwarded` entry value
+pub enum ForwardedValue<'a> {
+    ///Identifies node that passed request to the proxy
+    ///
+    ///This potentially can be the same as `For`
+    By(ForwardedNode<'a>),
+    ///Contains client's IP information
+    ///
+    ///This is normally what you're looking for when you need to identify client's original IP
+    For(ForwardedNode<'a>),
+    ///Original value of `Host` header
+    Host(&'a str),
+    ///String with protocol name
+    ///
+    ///https://datatracker.ietf.org/doc/html/rfc7239#section-5.4
+    Protocol(&'a str)
+}
+
+///Iterator of `Forwarded` entry's components
+pub struct ForwardedEntryIter<'a> {
+    components: core::str::Split<'a, char>,
+}
+
+impl<'a> ForwardedEntryIter<'a> {
+    ///Parses single entry within `Forwarded` header
+    ///
+    ///It performs no error checking, ignoring invalid values, as it assumes you parse valid `Forwarded` header
+    ///
+    ///Values within entry is separated by `;`
+    ///
+    ///This iterator returns [ForwardedValue](enum.ForwardedValue.html)
+    pub fn parse_entry(value: &'a str) -> Self {
+        Self {
+            components: value.split(ENTRY_SEP)
+        }
     }
 }
 
-///Iterator over `node` components within `ForwardedNode` header
-pub struct ForwardedNodeIter<'a, I> {
-    components: I,
-    _lifetime: marker::PhantomData<&'a I>,
-}
+impl<'a> Iterator for ForwardedEntryIter<'a> {
+    type Item = ForwardedValue<'a>;
 
-impl<'a, I: Iterator<Item = &'a str> + 'a> Iterator for ForwardedNodeIter<'a, I> {
-    type Item = ForwardedNode<'a>;
     fn next(&mut self) -> Option<Self::Item> {
         while let Some(value) = self.components.next() {
-            let mut pairs = value.split('=');
+            let mut pairs = value.splitn(2, PAIR_SEP);
             let key = pairs.next().unwrap();
             if key.eq_ignore_ascii_case("for") {
-                if let Some(value) = pairs.next() {
-                    let value = value.trim_matches('"');
-                    if value.eq_ignore_ascii_case("unknown") {
-                        return Some(ForwardedNode::Unknown);
-                    }
-
-                    if let Some(mut ipv6) = value.strip_prefix('[') {
-                        if let Some(end_addr_idx) = ipv6.find(']') {
-                            ipv6 = &ipv6[..end_addr_idx];
-                            return Some(ForwardedNode::parse_name(ipv6));
-                        } else {
-                            return Some(ForwardedNode::Name(ipv6));
-                        }
-                    }
-
-                    let mut value = value.rsplit(':');
-                    let port_or_ip = value.next().unwrap();
-                    let ip = if let Some(ip) = value.next() {
-                        ip
-                    } else {
-                        port_or_ip
-                    };
-
-                    return Some(ForwardedNode::parse_name(ip));
+                if let Some(node) = pairs.next() {
+                    return Some(ForwardedValue::For(ForwardedNode::parse_node(node)))
+                }
+            } else if key.eq_ignore_ascii_case("by") {
+                if let Some(node) = pairs.next() {
+                    return Some(ForwardedValue::By(ForwardedNode::parse_node(node)))
+                }
+            } else if key.eq_ignore_ascii_case("proto") {
+                if let Some(proto) = pairs.next() {
+                    return Some(ForwardedValue::Protocol(proto))
+                }
+            } else if key.eq_ignore_ascii_case("host") {
+                if let Some(host) = pairs.next() {
+                    return Some(ForwardedValue::Host(host))
                 }
             }
         }
 
         None
+    }
+}
+
+///Iterator over entries components within `Forwarded` header
+pub struct ForwardedIter<'a, I> {
+    components: I,
+    _lifetime: marker::PhantomData<&'a I>,
+}
+
+impl<'a, I: Iterator<Item = &'a str> + 'a> Iterator for ForwardedIter<'a, I> {
+    type Item = ForwardedEntryIter<'a>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.components.next().map(ForwardedEntryIter::parse_entry)
+    }
+}
+
+///Iterator over `For` components within `Forwarded` header
+///
+///This is most likely what you need most of the time in order to determine client's actual IP, but
+///you can use [ForwardedIter](struct.ForwardedIter.html) when you need to iterate over all
+///components
+pub struct ForwardedForIter<'a, I> {
+    components: I,
+    _lifetime: marker::PhantomData<&'a I>,
+}
+
+impl<'a, I: Iterator<Item = &'a str> + 'a> Iterator for ForwardedForIter<'a, I> {
+    type Item = ForwardedNode<'a>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(value) = self.components.next() {
+            let mut pairs = value.splitn(2, PAIR_SEP);
+            let key = pairs.next().unwrap();
+            if key.eq_ignore_ascii_case("for") {
+                if let Some(node) = pairs.next() {
+                    return Some(ForwardedNode::parse_node(node))
+                }
+            }
+        }
+
+        None
+    }
+}
+
+#[inline(always)]
+///Parses provided string as `Forwarded` header
+///
+///It performs no error checking, ignoring invalid values, as it assumes you parse valid `Forwarded` header
+///
+///Every proxy's entry is separated by `,`
+///
+///This iterator returns iterator over individual proxy's entries within `value`
+pub fn parse_forwarded<'a>(value: &'a str) -> ForwardedIter<'a, impl Iterator<Item = &'a str>> {
+    ForwardedIter {
+        components: value.split(FORWARDED_SEP),
+        _lifetime: marker::PhantomData,
+    }
+}
+
+#[inline(always)]
+///Variant of [parse_forwarded](fn.parse_forwarded.html) that reverses order of output
+pub fn parse_forwarded_rev<'a>(value: &'a str) -> ForwardedIter<'a, impl Iterator<Item = &'a str>> {
+    ForwardedIter {
+        components: value.rsplit(FORWARDED_SEP),
+        _lifetime: marker::PhantomData,
+    }
+}
+
+#[inline(always)]
+///Parses provided string as `Forwarded` header returning all `For` nodes in order
+pub fn parse_forwarded_for<'a>(value: &'a str) -> ForwardedForIter<'a, impl Iterator<Item = &'a str>> {
+    ForwardedForIter {
+        components: value.split([FORWARDED_SEP, ENTRY_SEP]),
+        _lifetime: marker::PhantomData,
+    }
+}
+
+#[inline(always)]
+///Parses provided string as `Forwarded` header returning all `For` nodes in reverse order
+pub fn parse_forwarded_for_rev<'a>(value: &'a str) -> ForwardedForIter<'a, impl Iterator<Item = &'a str>> {
+    ForwardedForIter {
+        components: value.rsplit([FORWARDED_SEP, ENTRY_SEP]),
+        _lifetime: marker::PhantomData,
     }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,21 +1,109 @@
 use core::net::IpAddr;
 
-use http_ip::forwarded::{parse_forwarded_for, ForwardedNode};
+use http_ip::forwarded::{parse_forwarded_for, parse_forwarded_for_rev};
+use http_ip::forwarded::{parse_forwarded, parse_forwarded_rev};
+use http_ip::forwarded::{ForwardedNode, ForwardedValue};
 
 #[test]
-fn should_parse_forwarded_header() {
+fn should_parse_single_forwarded_entry() {
+    let mut ips = parse_forwarded("For=\"[2001:db8:cafe::17]:4711\"");
+    let mut entry = ips.next().expect("have single entry");
+    let ip = entry.next().unwrap();
+    let expected_ip: IpAddr = "2001:db8:cafe::17".parse().unwrap();
+    assert_eq!(ForwardedValue::For(ForwardedNode::Ip(expected_ip)), ip);
+    assert!(entry.next().is_none());
+    assert!(ips.next().is_none());
+}
+
+#[test]
+fn should_parse_two_forwarded_entries() {
+    let mut ips = parse_forwarded("By=\"[2001:db8:cafe::17]:4711\",For=127.0.0.1");
+    let mut entry = ips.next().expect("have single entry");
+
+    let ip = entry.next().unwrap();
+    let expected_ip: IpAddr = "2001:db8:cafe::17".parse().unwrap();
+    assert_eq!(ForwardedValue::By(ForwardedNode::Ip(expected_ip)), ip);
+
+    let mut entry = ips.next().expect("have single entry");
+    let ip = entry.next().unwrap();
+    let expected_ip: IpAddr = "127.0.0.1".parse().unwrap();
+    assert_eq!(ForwardedValue::For(ForwardedNode::Ip(expected_ip)), ip);
+
+    assert!(entry.next().is_none());
+    assert!(ips.next().is_none());
+}
+
+#[test]
+fn should_parse_multiple_forwarded_entries() {
+    let mut ips = parse_forwarded(
+        "By=\"[2001:db8:cafe::17]:4711\";For=127.0.0.1,For=unknown,For=_hidden",
+    );
+    let mut entry = ips.next().expect("have single entry");
+    let ip = entry.next().unwrap();
+    let expected_ip: IpAddr = "2001:db8:cafe::17".parse().unwrap();
+    assert_eq!(ForwardedValue::By(ForwardedNode::Ip(expected_ip)), ip);
+    let ip = entry.next().unwrap();
+    let expected_ip: IpAddr = "127.0.0.1".parse().unwrap();
+    assert_eq!(ForwardedValue::For(ForwardedNode::Ip(expected_ip)), ip);
+
+    let mut entry = ips.next().expect("have single entry");
+    let ip = entry.next().unwrap();
+    assert_eq!(ForwardedValue::For(ForwardedNode::Unknown), ip);
+
+    let mut entry = ips.next().expect("have single entry");
+    let ip = entry.next().unwrap();
+    assert_eq!(ForwardedValue::For(ForwardedNode::Name("_hidden")), ip);
+
+    assert!(entry.next().is_none());
+    assert!(ips.next().is_none());
+}
+
+#[test]
+fn should_parse_multiple_forwarded_entries_rev() {
+    let mut ips = parse_forwarded_rev(
+        "By=\"[2001:db8:cafe::17]:4711\";For=127.0.0.1,For=unknown,For=_hidden",
+    );
+
+    let mut entry = ips.next().expect("have single entry");
+    let ip = entry.next().unwrap();
+    assert_eq!(ForwardedValue::For(ForwardedNode::Name("_hidden")), ip);
+
+    let mut entry = ips.next().expect("have single entry");
+    let ip = entry.next().unwrap();
+    assert_eq!(ForwardedValue::For(ForwardedNode::Unknown), ip);
+
+    let mut entry = ips.next().expect("have single entry");
+    let ip = entry.next().unwrap();
+    let expected_ip: IpAddr = "2001:db8:cafe::17".parse().unwrap();
+    assert_eq!(ForwardedValue::By(ForwardedNode::Ip(expected_ip)), ip);
+    let ip = entry.next().unwrap();
+    let expected_ip: IpAddr = "127.0.0.1".parse().unwrap();
+    assert_eq!(ForwardedValue::For(ForwardedNode::Ip(expected_ip)), ip);
+
+    assert!(entry.next().is_none());
+    assert!(ips.next().is_none());
+}
+
+#[test]
+fn should_parse_single_entry_with_forwarded_for_simple() {
     let mut ips = parse_forwarded_for("For=\"[2001:db8:cafe::17]:4711\"");
     let ip = ips.next().unwrap();
     let expected_ip: IpAddr = "2001:db8:cafe::17".parse().unwrap();
     assert_eq!(ForwardedNode::Ip(expected_ip), ip);
     assert!(ips.next().is_none());
+}
 
+#[test]
+fn should_parse_single_entry_with_forwarded_for_multi() {
     let mut ips = parse_forwarded_for("By=\"[2001:db8:cafe::17]:4711\",For=127.0.0.1");
     let ip = ips.next().unwrap();
     let expected_ip: IpAddr = "127.0.0.1".parse().unwrap();
     assert_eq!(ForwardedNode::Ip(expected_ip), ip);
     assert!(ips.next().is_none());
+}
 
+#[test]
+fn should_parse_multiple_entries_with_forwarded_for() {
     let mut ips = parse_forwarded_for(
         "By=\"[2001:db8:cafe::17]:4711\";For=127.0.0.1,For=unknown,For=_hidden",
     );
@@ -28,4 +116,39 @@ fn should_parse_forwarded_header() {
 
     let ip = ips.next().unwrap();
     assert_eq!(ForwardedNode::Name("_hidden"), ip);
+}
+
+#[test]
+fn should_parse_single_entry_with_forwarded_for_simple_rev() {
+    let mut ips = parse_forwarded_for_rev("For=\"[2001:db8:cafe::17]:4711\"");
+    let ip = ips.next().unwrap();
+    let expected_ip: IpAddr = "2001:db8:cafe::17".parse().unwrap();
+    assert_eq!(ForwardedNode::Ip(expected_ip), ip);
+    assert!(ips.next().is_none());
+}
+
+#[test]
+fn should_parse_single_entry_with_forwarded_for_multi_rev() {
+    let mut ips = parse_forwarded_for_rev("By=\"[2001:db8:cafe::17]:4711\",For=127.0.0.1");
+    let ip = ips.next().unwrap();
+    let expected_ip: IpAddr = "127.0.0.1".parse().unwrap();
+    assert_eq!(ForwardedNode::Ip(expected_ip), ip);
+    assert!(ips.next().is_none());
+}
+
+#[test]
+fn should_parse_multiple_entries_with_forwarded_for_rev() {
+    let mut ips = parse_forwarded_for_rev(
+        "By=\"[2001:db8:cafe::17]:4711\";For=127.0.0.1,For=unknown,For=_hidden",
+    );
+
+    let ip = ips.next().unwrap();
+    assert_eq!(ForwardedNode::Name("_hidden"), ip);
+
+    let ip = ips.next().unwrap();
+    assert_eq!(ForwardedNode::Unknown, ip);
+
+    let ip = ips.next().unwrap();
+    let expected_ip: IpAddr = "127.0.0.1".parse().unwrap();
+    assert_eq!(ForwardedNode::Ip(expected_ip), ip);
 }
